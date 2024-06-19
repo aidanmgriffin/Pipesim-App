7d73ce5b68e5e5a8ce00c8a8745dad05e9081daa
Aidan Griffin <aidan.griffin@wsu.edu>
2024-06-18 17:29:57 -1000

    Customize rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,8 @@ release = '1.0.0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
+
+    'sphinx_rtd_theme',
 ]
 
 templates_path = ['_templates']
@@ -25,5 +27,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
+html_logo = './static/images/portrait-logo.png'
 html_static_path = ['_static']
